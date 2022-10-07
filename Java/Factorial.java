d5bb4f3a21976a108253d8a1459b17cbad83e2bb
Bhavna Panjwani <97038127+Bhavna-26@users.noreply.github.com>
2022-10-07 14:16:39 +0530

    added beginner friendly java programs

--- Java/Factorial.java
+++ Java/Factorial.java
@@ -1,23 +1,13 @@
-import java.util.Scanner;
-
-/**
- * Factorial
- */
-public class Factorial {
-
-    static int factorial(int a) {
-        if (a==0 || a==1) {
-            return 1;
-        }
-        else {
-            return factorial(a-1)*a;
-        }
-    }
-
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter a number: ");
-        System.out.println("Factorial = "+factorial(in.nextInt())); 
-        in.close();
-    }
+//calculating factorial
+import java.util.*;
+class Factorial{
+public static void main(String args[]){
+int n;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter a no");
+n=sc.nextInt();
+int fact=1;
+for(int i=1;i<=n;i++){fact*=i;}
+System.out.println("Factorial of"+n+"is"+fact);
+}
 }
